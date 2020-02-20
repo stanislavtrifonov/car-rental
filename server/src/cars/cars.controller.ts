55cd27c73703bb16d6d71a5e6e97ddaa619e4214
stanislavtrifonov <stanislav.trifonov1@gmail.com>
2020-02-20 12:03:41 +0200

    cars controller/module/service + get()

---
+++ server/src/cars/cars.controller.ts
@@ -0,0 +1,32 @@
+import {
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    UseInterceptors,
+    Post,
+    Body,
+    UseGuards,
+    ValidationPipe,
+    Put,
+    Param,
+    Delete,
+    UsePipes,
+  } from '@nestjs/common';
+  
+import { CarsService } from './cars.service';
+import { Car } from '../database/entities/car.entity';
+
+
+@Controller('cars')
+export class CarsController {
+    public constructor(private readonly carsService: CarsService) { }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    public async getAllAvailableCars () {
+      const allAvailableCars: Car[] = await this.carsService.getAllAvailableCars();
+
+      return allAvailableCars;
+    }
+}
---
+++ server/src/cars/cars.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Car } from '../database/entities/car.entity';
+import { Repository } from 'typeorm';
+import { Class } from '../database/entities/class.entity';
+
+
+@Injectable()
+export class CarsService {
+    public constructor(
+        @InjectRepository(Car) private readonly carsRepository: Repository<Car>,
+        @InjectRepository(Class) private readonly classesRepository: Repository<Class>,
+    ) { }
+
+    public async getAllAvailableCars(): Promise<Car[]> {
+        const allCarsData: Car[] = await this.carsRepository.find({
+            where: {
+                isBorrowed: false,
+                isDeleted: false,
+            },
+            relations: ['className'],
+        });
+
+        return allCarsData;
+    }
+
+}
