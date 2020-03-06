1066dcdec6489a6cc25a6411b404357da87714d8
stanislavtrifonov <stanislav.trifonov1@gmail.com>
2020-03-06 16:57:03 +0200

    nightwatch test - working

--- client/tests/nightwatchTest.js
+++ client/tests/nightwatchTest.js
@@ -1,6 +1,6 @@
 const moment = require('moment');
 
-const dateTest = moment(new Date()).format('MM/DD/YYYY');
+const dateTest = (moment(new Date(), 'MM/DD/YYYY').add(5, 'days')).format('MM/DD/YYYY');
 
 
 module.exports = {
@@ -12,27 +12,38 @@ module.exports = {
       .assert.visible('.form-control')
       .assert.visible('.carsPage')
       .assert.visible('.dashboardContainer')
+      .assert.urlContains('/dashboard')
       .click('.carsPage')
+      .setValue('input[type=text]', 'Lada')
+      .click('.searchBtn')
+      .assert.urlContains('/home')
+      .assert.not.elementPresent('.card')
+      .clearValue('input[type=text]')
       .setValue('input[type=text]', 'Ford')
       .click('.searchBtn')
       .assert.visible('.container')
       .assert.visible('.card')
       .assert.visible('.checkoutBtn')
       .click('.checkoutBtn')
+      .assert.urlContains('/cars')
       .assert.visible('.firstNameField')
+      .assert.value('.firstNameField', 'Mark')
       .clearValue('.firstNameField')
       .setValue('.firstNameField', 'Ivan')
+      .assert.value('.firstNameField', 'Ivan')
       .clearValue('.lastNameField')
       .setValue('.lastNameField', 'Georgiev')
       .clearValue('.ageField')
       .setValue('.ageField', '25')
       .setValue('.dateField', dateTest)
       .click('.bookCarBtn')
+      .assert.visible('.dashboardContainer')
+      .assert.containsText('.dashboardContainer', 'Ford')
       .clearValue('input[type=text]')
       .click('.carsPage')
       .setValue('input[type=text]', 'Ford')
       .click('.searchBtn')
-      .assert.containsText('.mainline-results', 'Ford')
+      .assert.not.elementPresent('.card')
       .end();
   },
 };
